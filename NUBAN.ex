4c11b7cc6aed03461ae8f4a67a4fc96ffcc3b463
Uk Chukundah <ukchukundah@gmail.com>
2017-07-14 07:37:40 +0100

    Use more pipes.

--- NUBAN.ex
+++ NUBAN.ex
@@ -1,30 +1,31 @@
 defmodule NUBAN do
-
-  def is_valid_NUBAN(bank_code, account_number) do
-    is_valid_NUBAN(String.trim(bank_code) <> String.trim(account_number))
+  def valid?(bank_code, account_number) do
+    valid?(String.trim(bank_code) <> String.trim(account_number))
   end
   
-  def is_valid_NUBAN(account_number) do
+  def valid?(account_number) do
     case String.length(account_number) do
-      13 -> do_is_valid_NUBAN(account_number)
+      13 -> do_valid?(account_number)
       _ -> false
     end
   end
 
-  defp do_is_valid_NUBAN(account_number) do
-    [t1, s1, t2, t3, s2, t4, t5, s3, t6, t7, s4, t8, check_digit] =
+  defp do_valid?(account_number) do
+    {sum, check_digit} =
       account_number
       |> String.split("", trim: true)
-      |> Enum.map(fn x ->
-       {num, _} = Integer.parse(x)
-       num
-       end)
-
-    sum =
-      (t1 * 3) + (s1 * 7) + (t2 * 3) +
-      (t3 * 3) + (s2 * 7) + (t4 * 3) +
-      (t5 * 3) + (s3 * 7) + (t6 * 3) +
-      (t7 * 3) + (s4 * 7) + (t8 * 3)
+      |> Stream.map(fn x -> Integer.parse(x) |> elem(0) end)
+      |> Stream.with_index
+      |> Stream.map(fn
+        {num, idx} when idx in [1, 4, 7, 10] -> {num * 7, idx}
+        {num, 12} -> {num, 12}
+        {num, idx} -> {num * 3, idx}
+      end)
+     |> Enum.to_list
+     |> Enum.reduce({0, 0}, fn 
+        {num, 12}, {sum, _} -> {sum, num}
+        {num, x}, {sum, _} -> {sum + num, x}    
+     end)
 
     case rem(sum, 10) do
       x when (10 - check_digit) == x -> true
